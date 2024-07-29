db6c7b257652dc70a81e0a25c40b069bd8d8b9ae
Mark Maksi <mmaksi.dev@gmail.com>
2024-07-29 15:34:58 +0200

    started building the form to ask questions

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,25 @@
+import { Questions } from '@/components/forms/Questions';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const AskQuestions = () => {
-  return <div>AskQuestions</div>;
+const Page = async () => {
+  // const { userId } = auth();
+
+  const userId = '123456789';
+  if (!userId) redirect('/sign-in');
+
+  // const mongoUser = await getUserById({ userId });
+
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>
+        Ask a question
+      </h1>
+      <div className='mt-9'>
+        <Questions />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestions;
+export default Page;
