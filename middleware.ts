0df5851998c2d7c2c071a12171f004db7938f6f4
Mark Maksi <mmaksi.dev@gmail.com>
2024-08-06 08:14:45 +0200

    fixed signing in webhook error

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+} from '@clerk/nextjs/server';
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api/webhooks(.*)',
+]);
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware();
+export default clerkMiddleware((auth, request) => {
+  const { redirectToSignIn, userId } = auth();
+  if (!isPublicRoute(request) && !userId) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
