32651935a705b27dc37058c9e486ee318fc77f59
Mark Maksi <mmaksi.dev@gmail.com>
2024-08-06 03:33:17 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/api/webhooks/(.*)',
+  '/api/webhooks(.*)',
 ]);
 
 export default clerkMiddleware((auth, request) => {
